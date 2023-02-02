62e74383ee2a79c1a47e2f4817e6b675614d0ca0
Armillion <patrykpga1@gmail.com>
2023-02-02 20:45:54 +0100

    test

--- technical-task/technical-task.cpp
+++ technical-task/technical-task.cpp
@@ -7,6 +7,13 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello jebaczu." << endl;
+	for (int i = 0; i < 10; i++)
+		cout << "*" << endl;
+	
+	cout << "Hello world." << endl;
+
+	for (int i = 0; i < 10; i++)
+		cout << "*" << endl;
+
 	return 0;
 }
