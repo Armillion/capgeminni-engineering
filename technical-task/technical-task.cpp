e9893ec5334676778570c708ea244b507a8e32a4
Armillion <patrykpga1@gmail.com>
2023-02-02 20:10:43 +0100

    Forgot that one

---
+++ technical-task/technical-task.cpp
@@ -0,0 +1,12 @@
+﻿// technical-task.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "technical-task.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello jebaczu." << endl;
+	return 0;
+}
