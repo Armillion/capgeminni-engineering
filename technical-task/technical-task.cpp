f0c52d25ea3412430fdb4a3b2d354549302fffbc
Armillion <patrykpga1@gmail.com>
2023-02-03 15:40:42 +0100

    poprawiona wersja

--- technical-task/technical-task.cpp
+++ technical-task/technical-task.cpp
@@ -7,17 +7,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
-	for (int i = 0; i < 10; i++)
-		cout << "*" << endl;
-	
-	cout << "Hello world." << endl;
-
-	for (int i = 0; i < 10; i++)
-		cout << "*" << endl;
-
-=======
 	cout << "Hello." << endl;
->>>>>>> df66d58 (zmiany)
 	return 0;
 }
